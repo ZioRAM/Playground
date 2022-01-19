bda3af551a5d9a356253aa540f17bb9bb3af75e4
ZioRAM <ezequieladrianibarra@gmail.com>
2022-01-19 18:30:40 -0300

    Visueal deteails

--- Playground/JS/app.js
+++ Playground/JS/app.js
@@ -27,24 +27,21 @@ funcionNueva()
 Vue.component('navbar', {
     template: `<nav class="navbar navbar-expand-lg navbar-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="../HTML/index.html">MARVEL CAVE</a>
+      <a id="navy" class="navbar-brand" href="../HTML/index.html">MARVEL CAVE</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item dropdown">
+        <ul id="navy" class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li id="navy" class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
+              What's in here?
             </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Movies</a></li>
-              <li><a class="dropdown-item" href="#">Actors</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Characters</a></li>
+            <ul id="navy" class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a id="navy" class="dropdown-item" href="#">Movies</a></li>
+              <li><a id="navy" class="dropdown-item" href="#">Actors</a></li>
+              <li><hr id="navy" class="dropdown-divider"></li>
+              <li><a id="navy" class="dropdown-item" href="#">Characters</a></li>
             </ul>
           </li>
         </ul>
