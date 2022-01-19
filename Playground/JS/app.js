7c0877c7224c991fb0ea6f652e5519eca9f06e9e
ZioRAM <ezequieladrianibarra@gmail.com>
2022-01-19 17:57:28 -0300

    fix2

--- Playground/JS/app.js
+++ Playground/JS/app.js
@@ -4,7 +4,6 @@
 // const dataj=await fetch(api_url);
 // const datar=await dataj.json()
 // return datar;
-<<<<<<< HEAD
 // }
 
 // fetched().then((datos)=>console.log(datos));
@@ -24,11 +23,7 @@ async function funcionNueva() {
   console.log(info)
 }
 funcionNueva()
-=======
-// };
 
-// fetched().then((datos)=>console.log(datos));
->>>>>>> 7163eb6ee60ece19cded705adfb574d359b19056
 
 var api_url= "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=171586c59cef540df3c5a68aeaffd948&hash=c862d1e875614852cac5ef31b0a169f5"
 var info = []
