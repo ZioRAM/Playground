4e236c85bfc9adedcfdb2fc9cb82b1103d98a1de
Meel98 <97709627+Meel98@users.noreply.github.com>
2022-01-18 23:20:16 -0300

    hecho

--- Playground/JS/app.js
+++ Playground/JS/app.js
@@ -9,9 +9,6 @@
 // fetched().then((datos)=>console.log(datos));
 
 
-
-holaaaa
-
 var api_url= "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=171586c59cef540df3c5a68aeaffd948&hash=c862d1e875614852cac5ef31b0a169f5"
 var info = []
 async function traerDatos() {
